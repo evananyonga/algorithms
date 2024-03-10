b18a91012678fb8952ee59b2cb90172787e685c1
evananyonga <evewish@gmail.com>
2024-03-10 17:27:08 +0300

    verify function

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -14,3 +14,14 @@ def recursive_binary_search(numlist, target):
                 return recursive_binary_search(numlist[mid_point+1:], target)
             else:
                 return recursive_binary_search(numlist[:mid_point], target)
+        
+            
+def verify(index):
+    print("Target found: ", index)
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = recursive_binary_search(my_list, 12)
+verify(result)
+
+result = recursive_binary_search(my_list, 6)
+verify(result)
